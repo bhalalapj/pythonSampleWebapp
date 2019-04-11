b8c8050f1dd2f0b3f7bd1b2fe10614ef11867f41
Piyush Bhalala <bhalalapj@gmail.com>
2019-04-11 18:59:58 +0530

    Add project files.

---
+++ PythonWebTestApp/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from pycricbuzz import Cricbuzz
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello! World, Welcome to flask python web app from visual studio 2019"
+
+
+
+@app.route('/cricbuzz')
+def circbuzz():
+    c = Cricbuzz()
+    matches = c.matches()
+
+    for match in matches:
+        try:
+            if match['type'] == 'T20' :
+                list.append(match)
+        except:
+            return "Error occured in interation"             
+
+    return json.dumps(list, indent=4)
+
+if __name__ == '__main__':
+    app.run('localhost',4449)
